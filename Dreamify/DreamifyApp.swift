434ff1d79794cfc12b60a28626450b0fbe9a513d
JoWi2404 <joeywiryawan2@gmail.com>
2023-06-21 15:45:14 +0700

    Initial commit

---
+++ Dreamify/DreamifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DreamifyApp.swift
+//  Dreamify
+//
+//  Created by Joey Wiryawan on 21/06/23.
+//
+
+import SwiftUI
+
+@main
+struct DreamifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
