3dd28b1dbc4571a92b1164ed206a96b603d8d202
Alexander-3112 <ajewe3112@gmail.com>
2023-06-26 16:47:50 +0700

    Update ContentView.swift

--- Dreamify/ContentView.swift
+++ Dreamify/ContentView.swift
@@ -8,8 +8,10 @@ struct OnboardingView1: View {
                     .resizable()
                     .aspectRatio(contentMode: .fill)
                     .edgesIgnoringSafeArea(.all)
+                
                 VStack {
                     Spacer()
+                    
                     Image("Bulan")
                         .resizable()
                         .aspectRatio(contentMode: .fit)
@@ -39,127 +41,123 @@ struct OnboardingView1: View {
                             .cornerRadius(10)
                             .padding(.horizontal)
                     }
+                    .padding(.bottom, 30) // Penyesuaian posisi tombol
                 }
                 .padding()
             }
             .statusBar(hidden: true)
         }
     }
-    
-    struct OnboardingView2: View {
-        var body: some View {
-            ZStack {
-                Image("Background")
+}
+
+struct OnboardingView2: View {
+    var body: some View {
+        ZStack {
+            Image("Background")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Spacer()
+                
+                Image("Onboarding_voice_siri")
                     .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .edgesIgnoringSafeArea(.all)
-                VStack {
-                    Spacer()
-                    
-                    Image("Onboarding_voice_siri")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .padding()
-                    
-                    Text("Voice Recognition")
-                        .foregroundColor(Color(hex: 0xF7E5B6))
-                        .font(.title)
-                        .fontWeight(.bold)
-                        .padding()
-                    
-                    Text("Take control of the story with your voice to avoid direct contact with your phone.")
-                        .foregroundColor(.white)
-                        .font(.body)
-                        .multilineTextAlignment(.center)
-                        .padding()
-                    
-                    Spacer()
-                    
-                    NavigationLink(destination: OnboardingView3()) {
-                        Text("Next")
-                            .font(.headline)
-                            .foregroundColor(.white)
-                            .padding()
-                            .frame(maxWidth: .infinity)
-                            .background(LinearGradient(gradient: Gradient(colors: [Color.purple, Color.blue]), startPoint: .leading, endPoint: .trailing))
-                            .cornerRadius(10)
-                            .padding(.horizontal)
-                    }
-                }
-                .padding()
-            }
-        }
-        
-        struct OnboardingView3: View {
-            var body: some View {
-                ZStack {
-                    Image("Background")
-                        .resizable()
-                        .aspectRatio(contentMode: .fill)
-                        .edgesIgnoringSafeArea(.all)
-                VStack {
-                        Spacer()
-                        
-                        Image("Onboarding_focus_mode")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .padding()
-                        
-                        Text("Focus State Activation")
-                            .foregroundColor(Color(hex: 0xF7E5B6))
-                            .font(.title)
-                            .fontWeight(.bold)
-                            .padding()
-                        
-                        Text("It will be automatically turned on when a story gets played to provide comfort for your sleep.")
-                            .foregroundColor(.white)
-                            .font(.body)
-                            .multilineTextAlignment(.center)
-                            .padding()
-                        
-                        Spacer()
-                        
-                        NavigationLink(destination: Library2View()) {
-                            Text("Get Started")
-                                .font(.headline)
-                                .foregroundColor(.white)
-                                .padding()
-                                .frame(maxWidth: .infinity)
-                                .background(LinearGradient(gradient: Gradient(colors: [Color.purple, Color.blue]), startPoint: .leading, endPoint: .trailing))
-                                .cornerRadius(10)
-                                .padding(.horizontal)
-                        }
-                    }
+                    .aspectRatio(contentMode: .fit)
                     .padding()
-                }
-            }
-            
-            struct ContentView: View {
-                var body: some View {
-                    Text("Main Content")
-                        .font(.title)
-                        .foregroundColor(.black)
+                
+                Text("Voice Recognition")
+                    .foregroundColor(Color(hex: 0xF7E5B6))
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .padding()
+                
+                Text("Take control of the story with your voice to avoid direct contact with your phone.")
+                    .foregroundColor(.white)
+                    .font(.body)
+                    .multilineTextAlignment(.center)
+                    .padding()
+                
+                Spacer()
+                
+                NavigationLink(destination: OnboardingView3()) {
+                    Text("Next")
+                        .font(.headline)
+                        .foregroundColor(.white)
                         .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(LinearGradient(gradient: Gradient(colors: [Color.purple, Color.blue]), startPoint: .leading, endPoint: .trailing))
+                        .cornerRadius(10)
+                        .padding(.horizontal)
                 }
+                .padding(.bottom, 50) // Penyesuaian posisi tombol
             }
-            
-            struct OnboardingView1_Previews: PreviewProvider {
-                static var previews: some View {
-                    OnboardingView1()
-                }
-            }
-            
-            struct OnboardingView2_Previews: PreviewProvider {
-                static var previews: some View {
-                    OnboardingView2()
-                }
-            }
-            
-            struct OnboardingView3_Previews: PreviewProvider {
-                static var previews: some View {
-                    OnboardingView3()
-                }
-            }
+            .padding()
         }
+    }
+}
+
+struct OnboardingView3: View {
+    var body: some View {
+        ZStack {
+            Image("Background")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Spacer()
+                
+                Image("Onboarding_focus_mode")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .padding()
+                
+                Text("Focus State Activation")
+                    .foregroundColor(Color(hex: 0xF7E5B6))
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .padding()
+                
+                Text("It will be automatically turned on when a story gets played to provide comfort for your sleep.")
+                    .foregroundColor(.white)
+                    .font(.body)
+                    .multilineTextAlignment(.center)
+                    .padding()
+                
+                Spacer()
+                
+                NavigationLink(destination: Library2View()) {
+                    Text("Get Started")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(LinearGradient(gradient: Gradient(colors: [Color.purple, Color.blue]), startPoint: .leading, endPoint: .trailing))
+                        .cornerRadius(10)
+                        .padding(.horizontal)
+                }
+                .padding(.bottom, 50) // Penyesuaian posisi tombol
+            }
+            .padding()
+        }
+    }
+}
+
+struct OnboardingView1_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView1()
+    }
+}
+
+struct OnboardingView2_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView2()
+    }
+}
+
+struct OnboardingView3_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView3()
     }
 }
