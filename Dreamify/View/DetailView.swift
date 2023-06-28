1c9128687c578caf60ffcd9366ebbbbec6e6f38c
kellyflorences <77277696+kellyflorences@users.noreply.github.com>
2023-06-28 15:21:20 +0700

    audio combined w UI

--- Dreamify/View/DetailView.swift
+++ Dreamify/View/DetailView.swift
@@ -6,127 +6,247 @@
 //
 
 import SwiftUI
+import AVKit
 
-//struct DetailView: View {
-//    var body: some View {
-//        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-//    }
-//}
-//
-//struct DetailView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        DetailView()
-//    }
-//}
-
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView(imageData: ImageData(name: "Enchanted Garden", title: "Enchanted Garden", audio: "audio1", caption: "Narrator Volume"))
+    }
+}
 
 struct DetailView: View {
     let imageData: ImageData
     
-    @State private var isPlaying: Bool = false
+//    @State private var isPlaying: Bool = false
     @State private var playbackProgress: Float = 0.5
     @State private var isLiked: Bool = false
     
+    @State var audioBrownNoise: AVAudioPlayer!
+    @State var audioNarration: AVAudioPlayer!
+    @State var currentSec : Float = 0.0
+    @State var audioDuration:Float = 0.0
+    @State var brownDuration:Float = 0.0
+    @State var volume = 1.0
+    @State var isEditing = false
+    @State var isEditingTime = false
+    @State var remainingDuration = ""
+    @State var currentDuration = ""
+    @State var isPlaying = false
+    @State var narrationIsPlaying = false
+    @State private var timer: Timer? = nil
+    
     var body: some View {
         NavigationStack {
-            VStack(spacing: 30) {
-                Image(imageData.name)
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .frame(width: 300, height: 300)
-                    .clipShape(RoundedRectangle(cornerRadius: 16))
-                    .padding(.top, 20) // Menambahkan jarak pada bagian atas gambar
-                    .padding()
-                    .padding()
-                
-                HStack {
-                    Spacer()
-                            .frame(width: 30)
-                    Text(imageData.title)
-                        .font(.title)
-                        .fontWeight(.bold)
-                        .foregroundColor(Color(hex: 0xF7E5B6))
+            VStack{
+                VStack() {
+                    Image(imageData.name)
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 350, height: 350)
+                        .clipShape(RoundedRectangle(cornerRadius: 16))
+                        .padding(.top, 20) // Menambahkan jarak pada bagian atas gambar
+                    
                     Spacer()
                     
-                    Button(action: {
-                        isLiked.toggle()
-                        // Implement action for like button
-                    }) {
-                        Image(systemName: isLiked ? "heart.fill" : "heart")
+                    HStack{
+                        Text(imageData.title)
                             .font(.title)
-                            .foregroundColor(isLiked ? .red : .gray)
-                    }
-                    .buttonStyle(BorderlessButtonStyle())
-                    
-                }
-                .padding(.horizontal)
-                
-                Slider(value: $playbackProgress)
-                    .accentColor(.green)
-                    .frame(width: UIScreen.main.bounds.width - 100) // Menyesuaikan panjang slider
-                    .padding(.horizontal)
-
-                
-                HStack {
-                    Spacer()
-                    
-                    Button(action: {
-                        // Implement action for rewind button
-                    }) {
-                        Image(systemName: "gobackward.15")
-                            .font(.largeTitle)
-                    }
-                    .buttonStyle(BorderlessButtonStyle())
-                    .foregroundColor(Color(hex: 0xF7E5B6))
-                    
-                    Spacer()
-                    
-                    Button(action: {
-                        togglePlayback()
-                    }) {
-                        Image(systemName: isPlaying ? "pause.fill" : "play.fill")
-                            .font(.system(size: 50))
+                            .fontWeight(.bold)
                             .foregroundColor(Color(hex: 0xF7E5B6))
+                            .multilineTextAlignment(.leading)
+                        Spacer()
+                    }.padding(.top, 50)
+                    
+                    Slider(
+                        value: $currentSec,
+                        in: 0...brownDuration,
+                        //                        step: 0.001,
+                        onEditingChanged: { editing in
+                            isEditingTime = editing
+                            if(!isEditingTime){
+                                self.audioNarration.currentTime = TimeInterval(currentSec)
+                                self.audioBrownNoise.currentTime = TimeInterval(currentSec)
+                                print(currentSec, " ", audioDuration)
+                                updateTime()
+                                
+                                //condition when narration is done (only brown noise is playing, but the user rewind it from the slider)
+                                if(!narrationIsPlaying && isPlaying){
+                                    if(currentSec < audioDuration){
+                                        self.audioNarration.play()
+                                        self.audioNarration.currentTime = TimeInterval(currentSec - 15)
+                                        narrationIsPlaying = true
+                                    }
+                                }
+                            }
+                        }
+                    )
+                    .accentColor(.white)
+                    
+                    HStack{
+                        Text(currentDuration)
+                            .fontWeight(.bold)
+                            .foregroundColor(Color(white: 0.5))
+                        Spacer()
+                        Text(remainingDuration)
+                            .fontWeight(.bold)
+                            .foregroundColor(Color(white: 0.5))
                     }
-                    .buttonStyle(BorderlessButtonStyle())
+                    
+                    
+                    HStack{
+//                        Spacer()
+                        Button(action:{
+                            self.audioNarration.currentTime = TimeInterval(currentSec - 15)
+                            self.audioBrownNoise.currentTime = TimeInterval(currentSec - 15)
+                            updateTime()
+                            
+                            //condition when narration is done (only brown noise is
+                            //playing, but the user rewind it by 15 secs)
+                            if(!narrationIsPlaying && isPlaying){
+                                if(currentSec < audioDuration){
+                                    self.audioNarration.play()
+                                    self.audioNarration.currentTime = TimeInterval(currentSec - 15)
+                                    narrationIsPlaying = true
+                                }
+                            }
+                            
+                        }){
+                            Image(systemName: "gobackward.15")
+                                .resizable()
+                                .frame(width: 30, height: 30)
+                        }.foregroundColor(Color(hex: 0xF7E5B6))
+                        
+                        Spacer()
+                        
+                        if(isPlaying){
+                            Button(action: {
+                                print("pause")
+                                if(narrationIsPlaying){
+                                    self.audioNarration.pause()
+                                }
+                                self.audioBrownNoise.pause()
+                                isPlaying = false
+                                
+                                timer?.invalidate()
+                            }) {
+                                Image(systemName: "pause.fill").resizable()
+                                    .frame(width: 50, height: 50)
+                                    .aspectRatio(contentMode: .fit)
+                            }.foregroundColor(Color(hex: 0xF7E5B6))
+                        }else{
+                            Button(action: {
+                                print("play")
+                                self.audioBrownNoise.play()
+                                self.audioNarration.play()
+                                isPlaying = true
+                                narrationIsPlaying = true
+                                
+                                //update timer
+                                timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { _ in
+                                    updateTime()
+                                    print("current time: ", currentSec)
+                                    if (currentSec >= audioDuration) {
+                                        narrationIsPlaying = false
+                                        print("end")
+                                    }
+                                }
+                            }) {
+                                Image(systemName: "play.fill").resizable()
+                                    .frame(width: 50, height: 50)
+                                    .aspectRatio(contentMode: .fit)
+                            }.foregroundColor(Color(hex: 0xF7E5B6))
+                        }
+                        Spacer()
+                        Button(action:{
+                            self.audioNarration.currentTime = TimeInterval(currentSec + 30)
+                            self.audioBrownNoise.currentTime = TimeInterval(currentSec + 30)
+                            updateTime()
+                        }){
+                            Image(systemName: "goforward.30")
+                                .resizable()
+                                .frame(width: 30, height: 30)
+                        }.foregroundColor(Color(hex: 0xF7E5B6))
+//                        Spacer()
+                    }.padding(.horizontal, 50)
                     
                     Spacer()
                     
-                    Button(action: {
-                        // Implement action for fast forward button
-                    }) {
-                        Image(systemName: "goforward.15")
-                            .font(.largeTitle)
+                    VStack(spacing: 0){
+                        Text(imageData.caption)
+                            .multilineTextAlignment(.center)
+                            .padding(.top)
+                            .fontWeight(.bold)
+                            .foregroundColor(Color(white: 0.5))
+                        
+                        Slider(
+                            value: $volume,
+                            in: 0...1,
+                            onEditingChanged: { editing in
+                                isEditing = editing
+                                if(!isEditing){
+                                    self.audioNarration.volume = Float(volume)
+                                }
+                            }
+                        )
+                        .padding(0)
+                        .accentColor(Color(white: 0.8))
+
                     }
-                    .buttonStyle(BorderlessButtonStyle())
-                    .foregroundColor(Color(hex: 0xF7E5B6))
                     
                     Spacer()
                 }
-                .padding(.horizontal)
-                .padding()
-                
-                Text(imageData.caption)
-                    .font(.headline)
-                    .foregroundColor(.white)
-                    .multilineTextAlignment(.center)
-                    .padding(.horizontal)
-                
-                Slider(value: $playbackProgress)
-                    .accentColor(.green)
-                    .frame(width: UIScreen.main.bounds.width - 100) // Menyesuaikan panjang slider
-                    .padding(.horizontal)
-
-                
-                Spacer()
-            }
-            .background(Color(hex: 0x574D82))
-            .navigationBarTitleDisplayMode(.inline)
-            .navigationBarHidden(true)
+                .navigationBarTitleDisplayMode(.inline)
+                .navigationBarHidden(true)
+                .padding(15)
+                .onAppear{
+                    let soundBrownNoise = Bundle.main.path(forResource: "treasure", ofType: "mp3")
+                    self.audioBrownNoise = try! AVAudioPlayer(contentsOf: URL(fileURLWithPath: soundBrownNoise!))
+                    let soundNarration = Bundle.main.path(forResource: "queencard", ofType: "mp3")
+                    self.audioNarration = try! AVAudioPlayer(contentsOf: URL(fileURLWithPath: soundNarration!))
+                    
+                    audioDuration = Float(audioNarration?.duration ?? 0)
+                    brownDuration = Float(audioBrownNoise?.duration ?? 0)
+                    
+                    // this is to compute and show remaining time
+                    let duration = Int((audioNarration?.duration ?? 0) - (audioNarration?.currentTime ?? 0))
+                    let minutes2 = duration/60
+                    let seconds2 = duration - minutes2 * 60
+                    remainingDuration = NSString(format: "%02d:%02d", minutes2,seconds2) as String
+                    
+                    //This is to show and compute current time
+                    let currentTime1 = Int((audioNarration?.currentTime)!)
+                    let minutes = currentTime1/60
+                    let seconds = currentTime1 - minutes * 60
+                    currentDuration = NSString(format: "%02d:%02d", minutes,seconds) as String
+                }
+            }.background(Color(red:0.19078, green:0.1647, blue:0.27058))
         }
     }
     
     private func togglePlayback() {
         isPlaying.toggle()
     }
+    
+    func updateTime(){
+        let duration = Int((audioBrownNoise?.duration ?? 0) - (audioBrownNoise?.currentTime ?? 0))
+        let minutes2 = duration/60
+        let seconds2 = duration - minutes2 * 60
+        remainingDuration = NSString(format: "%02d:%02d", minutes2,seconds2) as String
+        //        durationSec = Float((audioNarration?.duration ?? 0) - (audioNarration?.currentTime ?? 0))
+        
+        
+        let currentTime1 = Int((audioBrownNoise?.currentTime)!)
+        let minutes = currentTime1/60
+        let seconds = currentTime1 - minutes * 60
+        currentDuration = NSString(format: "%02d:%02d", minutes,seconds) as String
+        currentSec = Float((audioBrownNoise?.currentTime)!)
+        //        currentSecDouble = Double((audioNarration?.currentTime)!)
+    }
 }
+
+//struct DetailView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        DetailView()
+//    }
+//}
+
