fd1dedc825a10df01d74b24ec185553256ad4ea7
Alexander-3112 <ajewe3112@gmail.com>
2023-07-04 17:03:49 +0700

    Add voice recognition

--- Dreamify/DreamifyApp.swift
+++ Dreamify/DreamifyApp.swift
@@ -9,9 +9,11 @@ import SwiftUI
 
 @main
 struct DreamifyApp: App {
+    @StateObject private var speechRecognizer = SpeechRecognizer.shared
     var body: some Scene {
         WindowGroup {
             OnboardingView()
+                .environmentObject(speechRecognizer)
         }
     }
 }
--- Dreamify/View/Library 2.swift
+++ Dreamify/View/Library 2.swift
@@ -21,15 +21,15 @@ struct Library2View: View {
     @State var buttonPlay = "pause.fill"
     @State private var timer: Timer? = nil
     @State var currentSec: Float = 0.0
-
-
+    
+    
     let images = [
         ImageData(name: "Enchanted Garden", title: "Enchanted Garden", audio: "Enchanted_Garden", description: "When we think of a garden, we often imagine a place full of brightly glazed flowers along with the sound of pristine water fountain. Have you actually visited a garden who possess these traits? Even if you haven't, worry not. Cover your eyes and experience it yourself as you fall deep into slumber.", caption: "Rain on Rooftop"),
         ImageData(name: "Oceanic Venture", title: "Oceanic Venture", audio: "Oceanic_Venture", description: "Whatever lies deep in the ocean will forever remain a mystery. Have you ever wondered what it feels to sail through the magnificent colossus sized of water? Before you fall deeper into your unconscious state, let's have a quick venture, shall we?", caption: "Crashing Waves"),
         ImageData(name: "Merchant of Hearts", title: "Merchant of Hearts", audio: "Merchant_of_Hearts", description: "Adora was a reputable merchant for her expertise in potions, vials, and spells that could help with all kinds of love troubles. Despite being called the infamous Merchant of Hearts, Adora stumbled into a love problem with Mark, the guy whom she fell in love with. Can her potion brewing and enchantments be able to fix it? ", caption: "Rain & Thunder"),
         ImageData(name: "Grumpy Dorian", title: "Grumpy Dorian", audio: "Grumpy_Dorian", description: "You spilled on his favorite jacket? Unforgivable! Dorian was merely a little kid with a big temper. Whoever got in his way, they’re deemed unforgivable by this grumpy little kid. Are you one of those people, perhaps?", caption: "Snowfall")
     ]
-
+    
     var body: some View {
         NavigationStack {
             VStack(alignment: .leading, spacing: 0){
@@ -53,12 +53,12 @@ struct Library2View: View {
                                         audioBrownNoise.pause()
                                         isPlaying = false
                                     }
-//                                    if(isPlaying){
-//
-//                                    }
+                                    //                                    if(isPlaying){
+                                    //
+                                    //                                    }
                                     nowPlayingTitle = image.title
-
-//                                    nowPlayingTitle =
+                                    
+                                    //                                    nowPlayingTitle =
                                 }) {
                                     VStack(spacing: 0) {
                                         Image(image.name)
@@ -68,7 +68,7 @@ struct Library2View: View {
                                             .clipped()
                                             .padding(.bottom, 0)
                                             .cornerRadius(11)
-
+                                        
                                         Rectangle()
                                             .padding(.top, 0)
                                             .foregroundColor(.clear)
@@ -91,21 +91,21 @@ struct Library2View: View {
                                                     }
                                                     .padding(.horizontal)
                                                     .padding(.top, 10)
-
+                                                    
                                                     Text(image.description)
                                                         .font(.caption)
                                                         .multilineTextAlignment(.leading)
                                                         .kerning(0.06)
                                                         .foregroundColor(.white)
                                                         .frame(width: 280, alignment: .leading)
-
+                                                    
                                                     Spacer()
-
+                                                    
                                                     HStack {
                                                         Text(image.caption)
                                                             .fontWeight(.semibold)
                                                             .font(.system(size: 11))
-//                                                            .font(Font.custom("SF Pro Text", size: 11).weight(.semibold))
+                                                        //                                                            .font(Font.custom("SF Pro Text", size: 11).weight(.semibold))
                                                             .foregroundColor(Color("darkYellow"))
                                                             .multilineTextAlignment(.center)
                                                             .padding(.horizontal, 10)
@@ -117,7 +117,7 @@ struct Library2View: View {
                                                                     .cornerRadius(10)
                                                             )
                                                         Spacer()
-
+                                                        
                                                         Button(action: {
                                                             // Implement play button action here
                                                         }) {
@@ -125,7 +125,7 @@ struct Library2View: View {
                                                                 Circle()
                                                                     .foregroundColor(Color.gray)
                                                                     .frame(width: 30, height: 30)
-
+                                                                
                                                                 Image(systemName: "play.fill")
                                                                     .foregroundColor(.white)
                                                                     .font(.title3)
@@ -133,8 +133,8 @@ struct Library2View: View {
                                                         }
                                                     }
                                                     .padding(.horizontal)
-
-
+                                                    
+                                                    
                                                     Spacer()
                                                 }
                                             )
@@ -152,7 +152,7 @@ struct Library2View: View {
                                     DetailView(imageData: $selectedStory, audioBrownNoise: $audioBrownNoise, audioNarration: $audioNarration, isPlaying: $isPlaying, currentSec: $currentSec)
                                 }
                             }
-
+                            
                         }
                         .padding(.horizontal)
                     }
@@ -211,9 +211,9 @@ struct Library2View: View {
             .scrollContentBackground(.hidden)
             .onAppear{
                 if(isPlaying){
-//                    timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { _ in
-//                        print("current time: ", audioBrownNoise.currentTime)
-//                    }
+                    //                    timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { _ in
+                    //                        print("current time: ", audioBrownNoise.currentTime)
+                    //                    }
                 }
             }
         }
